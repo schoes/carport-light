8d74b33a0a4e08b4ad1e6aa3a9f20213a418203a
Sven Schoeni <sven.schoeni@gmail.com>
2016-10-10 22:09:29 +0200

    add Faslet Lib

--- car-port-light/car-port-light.ino
+++ car-port-light/car-port-light.ino
@@ -1,44 +1,48 @@
-#include <Adafruit_NeoPixel.h>
-
+ #include <FastLED.h>
+//LED WS2811
 #define NEOPIXEL_PIN 6
-#define LED 50
-#define PIR_PIN 2
-#define STATUS_PIN 13
+#define NUM_LEDS 300
 #define LED_DELAY 50
+#define LED_TYPE WS2811
+#define COLOR_ORDER GRB
+#define BRIGHTNESS  64
+#define TEMPERATURE_1 Candle
+CRGB leds[NUM_LEDS];
+// MOTION DETECTION
+#define PIR_PIN_ENTRANCE 2
+#define PIR_PIN_DRIVE_IN 2
+//LIGHT INTENSE DETECTION
+#define PIN_LIGHT_DETECTION_ENTRANCE 9
+#define PIN_LIGHT_DETECTION_DRIVE_IN 10
+#define LIGHT_INTENSE_BREAKPOINT 70
 
 
-int timeout = LED_DELAY * LED + 100;
+
+int timeout = LED_DELAY * NUM_LEDS + 100;
 boolean motionDetected = false;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
-uint32_t warm_white = strip.Color(255, 197, 143);
-uint32_t off = strip.Color(0, 0, 0);
 void setup() {
-  pinMode(STATUS_PIN, OUTPUT);
-  pinMode(PIR_PIN, INPUT);
-
-  //set the strip to off
-  strip.setBrightness(64);
-  strip.begin();
-  strip.show();
+  delay(3000);
+  //SETUP LEDS
+  FastLED.addLeds<LED_TYPE, NEOPIXEL_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+  FastLED.setTemperature( TEMPERATURE_1 );
+  //SETUP MOTION DETECTION
+  pinMode(PIR_PIN_ENTRANCE, INPUT);
+  pinMode(PIR_PIN_DRIVE_IN, INPUT);
+  //SETUP LIGHT DETECTION
+  pinMode(PIN_LIGHT_DETECTION_ENTRANCE,INPUT);
+  pinMode(PIN_LIGHT_DETECTION_DRIVE_IN,INPUT);
 }
 
 void loop() {
-  digitalWrite(STATUS_PIN, digitalRead(PIR_PIN));
-
-  if (digitalRead(PIR_PIN) == HIGH && !motionDetected) {
+  if (digitalRead(PIR_PIN_ENTRANCE) == HIGH && !motionDetected) {
     motionDetected != motionDetected;
-    for (int n = 0; n < LED ; n++) {
-      strip.setPixelColor(n, warm_white);
-      strip.show();
-      delay(LED_DELAY);
+    for (int n = 0; n < NUM_LEDS ; n++) {
+      if(n%60 == 0){
+        FastLED.show();
+        delay(LED_DELAY);
+      }  
     }
-  }
-
-  delay(timeout);
-  for (int n = LED ; n >= 0 ; n--) {
-    strip.setPixelColor(n, off);
-    strip.show();
-    delay(LED_DELAY);
   }
 
 }
