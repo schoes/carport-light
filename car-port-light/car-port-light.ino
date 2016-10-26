1d8e1a9b6c0f43bac2103cecfffee808b136bdce
Sven Schoeni <sven.schoeni@gmail.com>
2016-10-26 22:10:32 +0200

    Add Light intensity sensor

--- car-port-light/car-port-light.ino
+++ car-port-light/car-port-light.ino
@@ -13,10 +13,10 @@ CRGB leds[NUM_LEDS];
 // MOTION DETECTION
 #define PIR_PIN_ENTRANCE 9
 #define PIR_PIN_DRIVE_IN 10
-//LIGHT INTENSE DETECTION
-#define PIN_LIGHT_DETECTION_ENTRANCE 9
+//LIGHT INTENSE DETECTION --> Analog INPUT
+#define PIN_LIGHT_DETECTION_ENTRANCE 0
 #define PIN_LIGHT_DETECTION_DRIVE_IN 10
-#define LIGHT_INTENSE_BREAKPOINT 70
+#define LIGHT_INTENSE_BREAKPOINT 20
 
 int timeout = LED_DELAY * NUM_LEDS + 100;
 boolean motionDetected = false;
@@ -33,42 +33,47 @@ void setup() {
   pinMode(PIR_PIN_ENTRANCE, INPUT);
   pinMode(PIR_PIN_DRIVE_IN, INPUT);
   //SETUP LIGHT DETECTION
-  pinMode(PIN_LIGHT_DETECTION_ENTRANCE, INPUT);
+  //pinMode(PIN_LIGHT_DETECTION_ENTRANCE, INPUT);
   pinMode(PIN_LIGHT_DETECTION_DRIVE_IN, INPUT);
 }
 
 void loop() {
 
   //FastLED.delay(1000 / UPDATES_PER_SECOND);
-  while (digitalRead(PIR_PIN_ENTRANCE) == HIGH) {
-    Serial.println("MOTION DETECTED");
-    if (brightness <= 255) {
-      brightness += fadeAmount;
-      Serial.println("brigthness:" + brightness);
+  int lightIntensity = analogRead(PIN_LIGHT_DETECTION_ENTRANCE);
+  //Serial.println(lightIntensity);
+  if (lightIntensity <= LIGHT_INTENSE_BREAKPOINT) {
 
-      for (int n = 0; n < NUM_LEDS ; n++) {
-        leds[n] = CRGB::Green;
-        leds[n].fadeLightBy(brightness);
+    while (digitalRead(PIR_PIN_ENTRANCE) == HIGH) {
+      //Serial.println("MOTION DETECTED");
+      if (brightness <= 255) {
+        brightness += fadeAmount;
+        //Serial.println("brigthness:" + brightness);
+
+        for (int n = 0; n < NUM_LEDS ; n++) {
+          leds[n] = CRGB::Green;
+          leds[n].fadeLightBy(brightness);
+        }
+        FastLED.show();
+        delay(1000);
       }
-      FastLED.show();
-      delay(1000);
     }
-  }
 
-  while (digitalRead(PIR_PIN_ENTRANCE) == LOW) {
-    if (brightness > 0) {
-      brightness -= fadeAmount;
-      Serial.println("brigthness:" + brightness);
+    while (digitalRead(PIR_PIN_ENTRANCE) == LOW) {
+      if (brightness > 0) {
+        brightness -= fadeAmount;
+        //Serial.println("brigthness:" + brightness);
 
-      for (int n = 0; n < NUM_LEDS ; n++) {
-        leds[n] = CRGB::Black;
-        leds[n].fadeLightBy(brightness);
+        for (int n = 0; n < NUM_LEDS ; n++) {
+          leds[n] = CRGB::Black;
+          leds[n].fadeLightBy(brightness);
+        }
+        FastLED.show();
+        delay(1000);
       }
-      FastLED.show();
-      delay(1000);
     }
+    //FastLED.clear();
   }
-  FastLED.clear();
   //if () {
 
   //motionDetected != motionDetected;
