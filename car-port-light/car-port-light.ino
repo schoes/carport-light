d6d3a53c357741ae7834965dc50e039b7345b4df
Sven Schoeni <sven.schoeni@gmail.com>
2016-10-31 21:05:34 +0100

    read the burning time from a potentiometer

--- car-port-light/car-port-light.ino
+++ car-port-light/car-port-light.ino
@@ -2,33 +2,25 @@
 //LED WS2811
 #define NEOPIXEL_PIN 6
 #define NUM_LEDS 50
-#define LED_DELAY 50
-#define UPDATES_PER_SECOND 100
-#define LED_COUNT_ON 5
 #define LED_TYPE WS2811
 #define COLOR_ORDER GRB
-#define BRIGHTNESS  64
-#define TEMPERATURE_1 Candle
 CRGB leds[NUM_LEDS];
 // MOTION DETECTION
 #define PIR_PIN_ENTRANCE 9
 #define PIR_PIN_DRIVE_IN 10
 //LIGHT INTENSE DETECTION --> Analog INPUT
 #define PIN_LIGHT_DETECTION_ENTRANCE 0
+#define PIN_LIGHT_DURATION 1
 #define PIN_LIGHT_DETECTION_DRIVE_IN 10
 #define LIGHT_INTENSE_BREAKPOINT 200
 
-int timeout = LED_DELAY * NUM_LEDS + 100;
 boolean motionDetected = false;
-int brightness = 0;
 int fadeAmount = 5;
 void setup() {
   Serial.begin(9600);
   delay(3000);
   //SETUP LEDS
   FastLED.addLeds<LED_TYPE, NEOPIXEL_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  //  FastLED.setBrightness(  BRIGHTNESS );
-  //  FastLED.setTemperature( TEMPERATURE_1 );
   FastLED.clear();
   FastLED.show();
   //SETUP MOTION DETECTION
@@ -41,6 +33,7 @@ void setup() {
 
 void loop() {
   int lightIntensity = analogRead(PIN_LIGHT_DETECTION_ENTRANCE);
+  int burningDuration = calculateBurningDuration();
   if (lightIntensity <= LIGHT_INTENSE_BREAKPOINT) {
     while (digitalRead(PIR_PIN_ENTRANCE) == HIGH) {
       enableLight();
@@ -48,7 +41,7 @@ void loop() {
 
 
     while (digitalRead(PIR_PIN_ENTRANCE) == LOW) {
-      delay(1000);
+      delay(burningDuration);
       disableLight();
     }
   }
@@ -57,7 +50,7 @@ void loop() {
 void enableLight() {
   for (int fader = 0; fader < 255 ; fader += 5) {
     for (int n = 0; n < NUM_LEDS ; n++) {
-      leds[n] = TEMPERATURE_1;
+      leds[n] = Candle;
       leds[n].maximizeBrightness(fader);
       if (n % 10 == 0) {
         FastLED.show();
@@ -79,6 +72,10 @@ void disableLight() {
     }
     delay(20);
   }
+}
+
+int calculateBurningDuration(){
+  return (analogRead(PIN_LIGHT_DURATION) * 1000)/3;
 }
 
 
