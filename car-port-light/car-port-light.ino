61d7bcca407ac765b6f0a04da8aa3eacf61c2de7
Sven Schoeni <sven.schoeni@gmail.com>
2018-12-16 11:44:07 +0100

    add led tests

--- car-port-light/car-port-light.ino
+++ car-port-light/car-port-light.ino
@@ -10,7 +10,7 @@ CRGB leds[NUM_LEDS];
 //LIGHT INTENSE DETECTION --> Analog INPUT
 #define LIGHT_DETECTION_PIN_ENTRANCE A0
 int SAFETY_LED_TIME_OUT = 3000;
-int SHOW_LED_COLOR_TIME_OUT = 1000;
+int SHOW_LED_COLOR_TIME_OUT = 2000;
 int MIN_BURN_DURATION = 60000;
 int LIGHT_INTENSE_BREAKPOINT = 20;
 int fadeAmount = 5;
@@ -78,8 +78,22 @@ bool enableLight()
   {
     for (int n = 0; n < NUM_LEDS; n++)
     {
-      leds[n] = CRGB::Yellow;
+
+      if (n % 2 == 0)
+      {
+        leds[n] = CRGB::Gold;
+      }
+      else
+      {
+        leds[n] = CRGB::SandyBrown;
+      }
+
       leds[n].maximizeBrightness(fader);
+
+      if (n % 50 == 0)
+      {
+        delay(1000);
+      }
     }
     delay(20);
     FastLED.show();
