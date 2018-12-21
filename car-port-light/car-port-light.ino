da4bdcd29cfc28aebb26d0637f19d35c430c3b7c
Sven Schoeni <sven.schoeni@gmail.com>
2018-12-21 17:28:37 +0100

    add new logic

--- car-port-light/car-port-light.ino
+++ car-port-light/car-port-light.ino
@@ -7,21 +7,18 @@ Adafruit_NeoPixel strip;
 #define PIR_PIN_ENTRANCE 9
 //LIGHT INTENSE DETECTION --> Analog INPUT
 #define LIGHT_DETECTION_PIN_ENTRANCE A0
-int SAFETY_LED_TIME_OUT = 3000;
 int SHOW_LED_COLOR_TIME_OUT = 2000;
 int MIN_BURN_DURATION = 30000;
 int LIGHT_INTENSE_BREAKPOINT = 20;
-int fadeAmount = 5;
 int MAX_BRIGHTNESS = 200;
-boolean lightOn = false;
-bool motionDetected = false;
+bool lightOn = false;
+
 void setup()
 {
-
   //Serial.begin(9600);
-  setupMotionDetection();
   setupLeds();
   showLedColor();
+  setupMotionDetection();
 }
 
 void loop()
@@ -34,7 +31,7 @@ void loop()
       if (shouldEnableLight())
       {
         Serial.println("GET SOME LIGHT");
-        lightOn = enableLight();
+        enableLight();
       }
     }
   }
@@ -42,14 +39,49 @@ void loop()
   if (lightOn)
   {
     delay(getBurnDuration());
-    lightOn = disableLight();
+    disableLight();
   }
+}
+bool shouldEnableLight()
+{
+  int intensity = analogRead(LIGHT_DETECTION_PIN_ENTRANCE);
+  Serial.print("intensity:");
+  Serial.print(intensity);
+  Serial.println(" ");
+  return intensity <= LIGHT_INTENSE_BREAKPOINT;
+}
+
+void enableLight()
+{
+  Serial.println("Turn on the light");
+  for (int n = 0; n < NUM_LEDS; n++)
+  {
+    strip.setPixelColor(n, 255, 120, 5);
+    strip.show();
+    delay(50);
+  }
+  lightOn = true;
+}
+
+void disableLight()
+{
+  for (int n = NUM_LEDS; n >= 0; n--)
+  {
+    strip.setPixelColor(n, 0, 0, 0);
+    strip.show();
+    delay(50);
+  }
+  lightOn = false;
+}
+
+int getBurnDuration()
+{
+  return MIN_BURN_DURATION;
 }
 
 void setupLeds()
 {
   Serial.println("==SETUP THE LED STRIP==");
-  //SETUP LEDS
   pinMode(NEOPIXEL_PIN, OUTPUT);
   strip = Adafruit_NeoPixel(NUM_LEDS, NEOPIXEL_PIN, NEO_BRG);
   strip.begin();
@@ -66,46 +98,4 @@ void showLedColor()
 void setupMotionDetection()
 {
   pinMode(LIGHT_DETECTION_PIN_ENTRANCE, INPUT);
-}
-
-bool enableLight()
-{
-  Serial.println("Turn on the light");
-  for (int n = 0; n < NUM_LEDS; n++)
-  {
-    strip.setPixelColor(n, 255, 120, 5);
-    strip.show();
-    delay(50);
-  }
-}
-return true;
-}
-
-bool disableLight()
-{
-  for (int fader = 0; fader < MAX_BRIGHTNESS; fader += 5)
-  {
-    for (int n = NUM_LEDS; n >= 0; n--)
-    {
-      leds.setPixelColor(n, 0, 0, 0);
-      strip.show();
-      delay(50);
-    }
-  }
-
-  return false;
-}
-
-bool shouldEnableLight()
-{
-  int intensity = analogRead(LIGHT_DETECTION_PIN_ENTRANCE);
-  Serial.print("intensity:");
-  Serial.print(intensity);
-  Serial.println(" ");
-  return intensity <= LIGHT_INTENSE_BREAKPOINT;
-}
-
-int getBurnDuration()
-{
-  return MIN_BURN_DURATION;
 }
